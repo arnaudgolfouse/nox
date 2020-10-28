4c1186a46d8edc35ae52b80f96b1adbe4ee23119
Arnaud <arnaud.golfouse@free.fr>
2020-10-28 15:42:37 +0100

    add `memory_test` binary

---
+++ src/bin/memory_test.rs
@@ -0,0 +1,39 @@
+use nox::{
+    libraries,
+    runtime::{VMError, VM},
+};
+
+const _PROGRAM1: &str = "
+x = 4
+";
+
+const _PROGRAM2: &str = "
+t = { x = 8 }
+";
+
+const _PROGRAM3: &str = "
+for i in range(0, 4)
+    x = 8
+end
+";
+
+const _PROGRAM4: &str = "
+fn(y)
+    t = { f = fn(t, a) return t.x + a - y end, x = 8 }
+    t1 = t
+    for i in range(0, 2000)
+        t1 = { f = t1.f, x = 7, y = t.x * 12 }
+    end
+    t = nil
+    return t1.f(t1, 2)
+end
+(8)
+";
+
+fn main() -> Result<(), VMError> {
+    let mut vm = VM::new();
+    vm.import_all(libraries::prelude())?;
+    vm.parse_top_level(_PROGRAM4)?;
+    vm.run()?;
+    Ok(())
+}
