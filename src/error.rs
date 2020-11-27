d2c366621d753fb53a45740e2e793979f9edda50
Arnaud <arnaud.golfouse@free.fr>
2020-11-27 17:33:56 +0100

    Rework `errors.rs` to work with the new lexer

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,5 @@
-use colored::Colorize;
-use std::{
-    cmp::max,
-    fmt,
-    io::{BufRead, BufReader},
-    ops::Range,
-};
+use colored::{ColoredString, Colorize};
+use std::{fmt, iter::Enumerate, ops::Range, str::Lines};
 
 /// Describe what should the REPL do with this error.
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -16,7 +11,7 @@ pub enum Continue {
 }
 
 /// Helper function, return the width (in characters) of a number
-fn int_width(i: u32) -> usize {
+fn int_width(i: usize) -> usize {
     format!("{}", i).chars().count()
 }
 
@@ -49,6 +44,16 @@ pub(crate) fn display_error<T: fmt::Display, U: fmt::Display>(
     formatter: &mut fmt::Formatter,
 ) -> Result<(), fmt::Error> {
     display_error_internal(range, source, source_name, warning, formatter)?;
+    writeln!(formatter)?;
+    write!(
+        formatter,
+        "{} : ",
+        if warning {
+            "warning".yellow().bold()
+        } else {
+            "error".red().bold()
+        }
+    )?;
     write!(formatter, "{}", message)?;
     if let Some(note) = note {
         writeln!(formatter)?;
@@ -59,6 +64,7 @@ pub(crate) fn display_error<T: fmt::Display, U: fmt::Display>(
     }
 }
 
+/// Avoid code duplication
 fn display_error_internal(
     range: Range<usize>,
     source: &str,
@@ -66,235 +72,214 @@ fn display_error_internal(
     warning: bool,
     formatter: &mut fmt::Formatter,
 ) -> Result<(), fmt::Error> {
-    todo!()
-}
+    let mut lines = LinesRange::new(source, range).peekable();
+    let (line, line_index, line_cols) = lines.next().unwrap_or_default();
+    writeln!(formatter, "{}: {}", source_name, line_index)?;
 
-// Avoids code duplication
-/*fn display_error_internal(
-    range: Range<usize>,
-    source: &str,
-    source_name: &str,
-    warning: bool,
-    formatter: &mut fmt::Formatter,
-) -> Result<(), fmt::Error> {
-    let color = |x| {
-        if warning {
-            <&str>::yellow(x)
-        } else {
-            <&str>::red(x)
-        }
-    };
-    let color_str = |x: String| {
-        if warning {
-            <&str>::yellow(&x)
-        } else {
-            <&str>::red(&x)
-        }
-    };
-
-    // helper function to print a line with its number (and and optional error trail)
-    let print_line = |formatter: &mut fmt::Formatter,
-                      line: String,
-                      line_number: usize,
-                      number_width: usize,
-                      error_trail: bool|
-     -> Result<(), fmt::Error> {
-        write!(
-            formatter,
-            "{}",
-            if warning {
-                format!(
-                    "{line:<width$} | ",
-                    line = line_number + 1, // text usually start at line 1
-                    width = number_width
-                )
-                .yellow()
-            } else {
-                format!(
-                    "{line:<width$} | ",
-                    line = line_number + 1, // text usually start at line 1
-                    width = number_width
-                )
-                .red()
-            }
-        )?;
-        if error_trail {
-            write!(formatter, "{} ", color("|"))?;
-        }
-        for c in line.chars() {
-            if c == '\t' {
-                // tab as 4 spaces
-                formatter.write_str("    ")?
-            } else {
-                write!(formatter, "{}", c)?
-            }
-        }
-        writeln!(formatter)
-    };
-
-    // helper function to print a line with its number (and and optional error trail), as well
-    // as creating an underline of '^' between the specified bounds (`error_start` and
-    // `error_end`).
-    let print_line_underlined = |formatter: &mut fmt::Formatter,
-                                 line: String,
-                                 line_number: usize,
-                                 number_width: usize,
-                                 error_trail: bool,
-                                 error_start: usize,
-                                 error_end: usize|
-     -> Result<String, fmt::Error> {
-        write!(
-            formatter,
-            "{}",
-            if warning {
-                format!(
-                    "{line:<width$} | ",
-                    line = line_number + 1, // text usually start at line 1
-                    width = number_width
-                )
-                .yellow()
-            } else {
-                format!(
-                    "{line:<width$} | ",
-                    line = line_number + 1, // text usually start at line 1
-                    width = number_width
-                )
-                .red()
-            }
-        )?;
-        let mut underline = format!("{1:0$}   ", number_width, "");
-        if error_trail {
-            write!(formatter, "{}", color("| "))?;
-            underline.push_str("|_")
-        }
-        let mut column_max = 0;
-        for (i, c) in line.chars().enumerate() {
-            column_max += 1;
-            if c == '\t' {
-                // tab as 4 spaces
-                formatter.write_str("    ")?;
-                if i < error_end && i >= error_start {
-                    underline.push_str("____")
-                } else if i < error_start {
-                    underline.push_str("    ")
-                }
-            } else {
-                write!(formatter, "{}", c)?;
-                if i < error_end && i >= error_start {
-                    underline.push('_')
-                } else if i < error_start {
-                    underline.push(' ')
-                }
-            }
-            if i == error_end {
-                underline.push('^')
-            }
-        }
-        while error_end > column_max {
-            column_max += 1;
-            underline.push('_')
-        }
-        if error_end == column_max {
-            underline.push('^')
-        }
-        writeln!(formatter)?;
-        Ok(color_str(underline).to_string())
-    };
-
-    writeln!(
-        formatter,
-        "{}: {}:{}",
-        source_name,
-        range.start.line + 1,
-        range.start.column + 1
-    )?;
-
-    let multiline = range.start.line < range.end.line;
-    let number_width = max(
-        int_width(range.start.line + 1),
-        int_width(range.end.line + 1),
-    );
-    let reader = BufReader::new(source.as_bytes());
+    let multiline = lines.peek().is_some();
+    // take two more chars just in case, because we don't know the index of the last line yet.
+    let number_width = int_width(line_index + 1) + 2;
 
     if multiline {
-        let mut lines = reader
-            .lines()
-            .enumerate()
-            .skip(range.start.line as usize)
-            .take((range.end.line - range.start.line + 1) as usize);
-
         // first line
         {
-            let (i, first_line) = lines.next().unwrap();
-            let underline = print_line_underlined(
+            print_numbered_line(formatter, line, line_index, number_width, None, warning)?;
+            print_underline(
                 formatter,
-                first_line.unwrap(),
-                i,
-                number_width,
-                false,
-                0,
-                range.start.column.saturating_sub(1) as usize,
-            )?;
-            writeln!(formatter, "{}", underline)?
+                number_width + 3,
+                None,
+                0..line_cols.start,
+                '_',
+                '^',
+                warning,
+            )?
         }
 
-        // middle lines
-        let underline = loop {
-            let (i, line) = match lines.next() {
-                Some((i, line)) => {
-                    if i == range.end.line as usize {
-                        break print_line_underlined(
-                            formatter,
-                            line.unwrap(),
-                            i,
-                            number_width,
-                            true,
-                            0,
-                            range.end.column as usize,
-                        )?;
-                    }
-                    (i, line.unwrap())
-                }
-                None => unreachable!(),
-            };
-            print_line(formatter, line, i, number_width, true)?
-        };
-
-        // last line
-        writeln!(formatter, "{}", underline)?;
-    } else {
-        let line_number = range.start.line as usize;
-        let line = reader.lines().nth(line_number).unwrap().unwrap();
-        let underline = print_line_underlined(
-            formatter,
-            line,
-            line_number,
-            number_width,
-            false,
-            range.start.column as usize,
-            range.end.column as usize,
-        )?
-        .replace("_", "^");
-        writeln!(
-            formatter,
-            "{}",
-            if warning {
-                underline.yellow()
+        while let Some((line, line_index, line_cols)) = lines.next() {
+            if lines.peek().is_some() {
+                // middle lines
+                print_numbered_line(
+                    formatter,
+                    line,
+                    line_index,
+                    number_width,
+                    Some("| "),
+                    warning,
+                )?
             } else {
-                underline.red()
+                // last line
+                print_numbered_line(
+                    formatter,
+                    line,
+                    line_index,
+                    number_width,
+                    Some("| "),
+                    warning,
+                )?;
+                print_underline(
+                    formatter,
+                    number_width + 3,
+                    Some("|_"),
+                    0..line_cols.end,
+                    '_',
+                    '^',
+                    warning,
+                )?;
             }
+        }
+    } else {
+        print_numbered_line(formatter, line, line_index, number_width, None, warning)?;
+        print_underline(
+            formatter,
+            number_width + 3,
+            None,
+            line_cols,
+            '^',
+            '^',
+            warning,
         )?
     }
 
-    writeln!(formatter)?;
-    write!(
-        formatter,
-        "{} : ",
-        if warning {
-            "warning".yellow().bold()
-        } else {
-            "error".red().bold()
+    Ok(())
+}
+
+///
+/// It returns the line, the line number, and the span covered by the range
+/// inside this line.
+struct LinesRange<'source> {
+    lines: Enumerate<Lines<'source>>,
+    start_ptr: *const u8,
+    end_ptr: *const u8,
+    /// We want to return at least one line, even if the range is out of bounds.
+    at_least_one_line: bool,
+}
+
+impl<'source> LinesRange<'source> {
+    fn new(source: &'source str, range: Range<usize>) -> Self {
+        Self {
+            lines: source.lines().enumerate(),
+            start_ptr: (source.as_ptr() as usize + range.start) as *const u8,
+            end_ptr: (source.as_ptr() as usize + range.end) as *const u8,
+            at_least_one_line: false,
         }
+    }
+}
+
+impl<'source> Iterator for LinesRange<'source> {
+    type Item = (&'source str, usize, Range<usize>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (mut index, mut next_line) = self.lines.next()?;
+        while (next_line.as_ptr() as usize + next_line.len()) <= (self.start_ptr as usize) {
+            let (tmp_index, tmp_next_line) = match self.lines.next() {
+                Some((index, line)) => (index, line),
+                // wait ! check that we already returned at least one line !
+                None => {
+                    return if !self.at_least_one_line {
+                        self.at_least_one_line = true;
+                        let columns_range = (self.start_ptr as usize)
+                            .saturating_sub(next_line.as_ptr() as usize)
+                            ..(self.end_ptr as usize).saturating_sub(next_line.as_ptr() as usize);
+                        Some((next_line, index, columns_range))
+                    } else {
+                        None
+                    }
+                }
+            };
+            index = tmp_index;
+            next_line = tmp_next_line;
+        }
+        let next_line_ptr = next_line.as_ptr() as usize;
+        let columns_range = (self.start_ptr as usize).saturating_sub(next_line_ptr)
+            ..(self.end_ptr as usize).saturating_sub(next_line_ptr);
+        if next_line_ptr > (self.end_ptr as usize) {
+            if !self.at_least_one_line {
+                self.at_least_one_line = true;
+                Some((next_line, index, columns_range))
+            } else {
+                None
+            }
+        } else {
+            self.at_least_one_line = true;
+            Some((next_line, index, columns_range))
+        }
+    }
+}
+
+/// Color a string based on warning (yellow) or error (red).
+fn color(string: &str, warning: bool) -> ColoredString {
+    if warning {
+        string.yellow()
+    } else {
+        string.red()
+    }
+}
+
+/// Print an underline, and a newline
+///
+/// # Example
+///
+/// ```compile_fail
+/// print_underline(formatter, 3, 2..4, '_', '^', false)
+/// ```
+/// produces
+/// ```text
+/// "     _^"
+/// ```
+fn print_underline(
+    formatter: &mut fmt::Formatter,
+    offset: usize,
+    prefix: Option<&str>,
+    mut range: Range<usize>,
+    character: char,
+    last_char: char,
+    warning: bool,
+) -> fmt::Result {
+    if range.start == range.end {
+        range.end += 1
+    }
+    let start = offset + range.start;
+    let length = range.end.saturating_sub(range.start);
+    let mut underline = if let Some(prefix) = prefix {
+        prefix.to_owned()
+    } else {
+        String::new()
+    };
+    for _ in 0..length.saturating_sub(1) {
+        underline.push(character)
+    }
+    if length != 0 {
+        underline.push(last_char)
+    }
+    writeln!(
+        formatter,
+        "{underline:>start$}",
+        start = start + underline.len(),
+        underline = color(&underline, warning)
     )
 }
-*/
+
+/// Print the line number (colored), the eventual prefix (colored), the line,
+/// and a newline.
+fn print_numbered_line(
+    formatter: &mut fmt::Formatter,
+    line: &str,
+    line_number: usize,
+    number_width: usize,
+    prefix: Option<&str>,
+    warning: bool,
+) -> fmt::Result {
+    let colored_number = color(
+        &format!(
+            "{line:>width$} | ",
+            line = line_number + 1, // text usually start at line 1
+            width = number_width
+        ),
+        warning,
+    );
+    write!(formatter, "{}", colored_number)?;
+    if let Some(prefix) = prefix {
+        write!(formatter, "{}", color(prefix, warning))?
+    }
+    writeln!(formatter, "{}", line)
+}
